d22c7a70e12c3b708b61e6341bd996dd36f0a740
EWLeo <leontev.ew@gmail.com>
2021-11-29 11:02:17 +0300

    Frist Commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,68 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentRadioStation;
+    private int maxRadioStation = 9;
+    private int minRadioStation = 0;
+    private int currentSoundVolume;
+    private int maxSoundVolume = 10;
+    private int minSoundVolume = 0;
+
+    public int getCurrentRadioStation(){
+     return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation){
+       if (currentRadioStation<minRadioStation || currentRadioStation>maxRadioStation) {
+           return;
+       }
+       this.currentRadioStation = currentRadioStation;
+    }
+
+    public void increaseRadioStation() {
+        if (currentRadioStation < maxRadioStation) {
+            currentRadioStation = currentRadioStation + 1;
+        }
+        if (currentRadioStation>maxRadioStation){
+            currentRadioStation = minRadioStation;
+        }
+    }
+
+    public void decreaseRadioStation() {
+        if (currentRadioStation > minRadioStation) {
+            currentRadioStation = currentRadioStation - 1;
+        }
+        if (currentRadioStation<minRadioStation){
+            currentRadioStation = maxRadioStation;
+        }
+    }
+
+    public int getCurrentSoundVolume(){
+        return currentSoundVolume;
+    }
+
+    public void setCurrentSoundVolume(int currentSoundVolume){
+        if (currentSoundVolume<minSoundVolume || currentSoundVolume>maxSoundVolume) {
+            return;
+        }
+        this.currentSoundVolume = currentSoundVolume;
+    }
+
+    public void increaseSoundVolume() {
+        if (currentSoundVolume < maxSoundVolume) {
+            currentSoundVolume = currentSoundVolume + 1;
+        }
+        if (currentSoundVolume>maxSoundVolume){
+            currentSoundVolume = minSoundVolume;
+        }
+    }
+
+    public void decreaseSoundVolume() {
+        if (currentSoundVolume > minSoundVolume) {
+            currentSoundVolume = currentSoundVolume - 1;
+        }
+        if (currentSoundVolume<minSoundVolume){
+            currentSoundVolume = maxSoundVolume;
+        }
+    }
+}
